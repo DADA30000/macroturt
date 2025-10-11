1b3218842631168ec6019bad7c522dcf6ff887e7
DADA30000 <vadimhack.ru@gmail.com>
2025-10-11 23:24:27 +0300

    update flake

--- flake.nix
+++ flake.nix
@@ -1,5 +1,6 @@
 {
   inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     utils.url = "github:numtide/flake-utils";
   };
   outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
@@ -9,6 +10,12 @@
     {
       devShell = pkgs.mkShell {
         buildInputs = with pkgs; [
+           pkg-config 
+           libx11 
+           xorg.libXi 
+           libGL 
+           alsa-lib 
+           libxkbcommon
         ];
       };
     }
